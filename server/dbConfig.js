b37688e0f55fe1d1e0f74ced0be9cbc3cd5c250b
alater <alater@student.neumont.edu>
2015-11-19 11:49:50 -0700

    fixed db connection

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -2,5 +2,5 @@
  * Created by Neal yingtonB on 11/16/2015.
  */
 module.exports={
-    'url' : '//alater:math200@ds054298.mongolab.com:54298/crypim'// update string path to database
+    'url' : 'mongodb://cyphim:test@ds054298.mongolab.com:54298/crypim'// update string path to database
 }
